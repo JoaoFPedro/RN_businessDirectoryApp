1aa9ca446798dd05b9c463e933975951df23e99b
Joao Pedro <joao.fpsilva98@gmail.com>
2024-09-30 19:24:47 -0300

    chaging color items

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -11,15 +11,15 @@ export default function _layout() {
 <Tabs screenOptions={{headerShown: false, tabBarActiveTintColor: Colors.PRIMARY}} >
     <Tabs.Screen name='home' options={{
       tabBarLabel: 'Home',
-      tabBarIcon:({color}) => <FontAwesome name="home" size={24} color="black" />
+      tabBarIcon:({color}) => <FontAwesome name="home" size={24} color={color} />
     }}/>
     <Tabs.Screen name='explore' options={{
       tabBarLabel: 'Explore',
-      tabBarIcon: ({color}) => <MaterialIcons name="explore" size={24} color="black" />
+      tabBarIcon: ({color}) => <MaterialIcons name="explore" size={24} color={color} />
     }}/>
     <Tabs.Screen name='profile' options={{
       tabBarLabel: 'Profile',
-      tabBarIcon: ({color}) => <MaterialCommunityIcons name="face-man-profile" size={24} color="black" />
+      tabBarIcon: ({color}) => <MaterialCommunityIcons name="face-man-profile" size={24} color={color} />
     }}/>
 </Tabs>
   )
