5624a719ce25179a5e93fb46b7f910a4740823cd
Joao Pedro <joao.fpsilva98@gmail.com>
2024-09-30 19:07:03 -0300

    feat: tab bar icons and label

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,13 +1,25 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import { Tabs } from 'expo-router'
+import FontAwesome from '@expo/vector-icons/FontAwesome';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import MaterialCommunityIcons from '@expo/vector-icons/MaterialCommunityIcons';
 
 export default function _layout() {
   return (
 <Tabs screenOptions={{headerShown: false}} >
-    <Tabs.Screen name='home'/>
-    <Tabs.Screen name='explore'/>
-    <Tabs.Screen name='profile'/>
+    <Tabs.Screen name='home' options={{
+      tabBarLabel: 'Home',
+      tabBarIcon:({color}) => <FontAwesome name="home" size={24} color="black" />
+    }}/>
+    <Tabs.Screen name='explore' options={{
+      tabBarLabel: 'Explore',
+      tabBarIcon: ({color}) => <MaterialIcons name="explore" size={24} color="black" />
+    }}/>
+    <Tabs.Screen name='profile' options={{
+      tabBarLabel: 'Profile',
+      tabBarIcon: ({color}) => <MaterialCommunityIcons name="face-man-profile" size={24} color="black" />
+    }}/>
 </Tabs>
   )
 }
