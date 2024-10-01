c8cbf7408b033756c86fd6e4556d632a6f13d9d7
Joao Pedro <joao.fpsilva98@gmail.com>
2024-10-01 20:40:06 -0300

    feat: Initialize Login Screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,15 +1,32 @@
+import LoginScreen from "@/components/login-screen";
+import { ClerkProvider, SignedIn, SignedOut } from "@clerk/clerk-expo";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
+import { Text } from "react-native";
 
 export default function RootLayout() {
   useFonts({
-    'outfit-bold': require('./../assets/fonts/Outfit-Bold.ttf'),
-    'outfit-medium': require('./../assets/fonts/Outfit-Medium.ttf'),
-    'outfit-regular': require('./../assets/fonts/Outfit-Regular.ttf')
-  })
+    "outfit-bold": require("./../assets/fonts/Outfit-Bold.ttf"),
+    "outfit-medium": require("./../assets/fonts/Outfit-Medium.ttf"),
+    "outfit-regular": require("./../assets/fonts/Outfit-Regular.ttf"),
+  });
+  const publishableKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY!;
+
+  if (!publishableKey) {
+    throw new Error(
+      "Missing Publishable Key. Please set EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY in your .env"
+    );
+  }
   return (
-    <Stack>
-      <Stack.Screen name="(tabs)" options={{headerShown: false}}/>
-    </Stack>
+    <ClerkProvider publishableKey={publishableKey}>
+      <SignedIn>
+        <Stack>
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        </Stack>
+      </SignedIn>
+      <SignedOut>
+        <LoginScreen />
+      </SignedOut>
+    </ClerkProvider>
   );
 }
