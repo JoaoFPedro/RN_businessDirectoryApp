536506dad2e44bc698cd6fac5f51423c45589a9f
Joao Pedro <joao.fpsilva98@gmail.com>
2024-10-03 20:13:38 -0300

    feat: google oauth autentication

--- components/login-screen.tsx
+++ components/login-screen.tsx
@@ -1,9 +1,51 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Colors } from "@/constants/Colors";
 import { Button } from "./button";
+import * as WebBrowser from  'expo-web-browser'
+import { useOAuth } from "@clerk/clerk-expo";
 
+
+
+WebBrowser.maybeCompleteAuthSession()
 export default function LoginScreen() {
+
+ 
+
+  const [isLoading, setIsLoading] = useState(false)
+  const googleOAuth = useOAuth({strategy:'oauth_google'})
+  
+  async function onGoogleSignIn() {
+    console.log('ENTROU')
+    try {
+      
+      setIsLoading(true)
+      const oAuthFlow = await googleOAuth.startOAuthFlow()
+
+      if(oAuthFlow.authSessionResult?.type === 'success'){
+       if(oAuthFlow.setActive){
+        await oAuthFlow.setActive({session: oAuthFlow.createdSessionId})
+       }
+       else{
+        setIsLoading(false)
+       }
+      }
+    } catch (error) {
+      console.log(error)
+      
+    }
+    finally{
+      setIsLoading(false)
+    }
+  }
+  useEffect(() => {
+    WebBrowser.warmUpAsync()
+
+    return () => {
+      WebBrowser.coolDownAsync()
+    }
+  },[])
+  
   return (
     <View style={styles.container}>
       <Image
@@ -30,8 +72,9 @@ export default function LoginScreen() {
             marginVertical: 15,
             padding: 12,
           }}
+          onPress={onGoogleSignIn}
         >
-           <Button title="Let's get started"/>
+           <Button title="Let's get started" />
             
            
         </TouchableOpacity>
