e0dfe8365f417910a0d3bce4630db4a42327fff7
Joao Pedro <joao.fpsilva98@gmail.com>
2024-10-08 19:34:47 -0300

    feat/ adding icon

--- components/search-bar.tsx
+++ components/search-bar.tsx
@@ -1,12 +1,21 @@
-import { View, Text, TextInput } from 'react-native'
-import React from 'react'
-
+import { View, Text, TextInput } from "react-native";
+import React from "react";
+import Feather from "@expo/vector-icons/Feather";
 export default function SearchBar() {
   return (
-    <View>
-      
-        <TextInput placeholder='Search...' style={{backgroundColor:'white', padding: 4}}></TextInput>
-     
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        width: "100%",
+        backgroundColor: "white",
+        alignItems: "center",
+        paddingHorizontal: 2,
+        borderRadius: 8,
+      }}
+    >
+      <Feather name="search" size={24} color="black" />
+      <TextInput placeholder="Search..." style={{ padding: 4 }}></TextInput>
     </View>
-  )
+  );
 }
