8456824aedd55c18bfcdbc37daa5a2e51cc4b73b
Joao Pedro <joao.fpsilva98@gmail.com>
2024-10-09 18:08:47 -0300

    refac: changing to css object

--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,4 +1,4 @@
-import { View, Text, Image, TextInput } from "react-native";
+import { View, Text, Image, TextInput, StyleSheet } from "react-native";
 import React from "react";
 import { useUser } from "@clerk/clerk-expo";
 import SearchBar from "@/components/search-bar";
@@ -6,20 +6,43 @@ import SearchBar from "@/components/search-bar";
 export default function home() {
   const user = useUser();
   return (
-    <View style={{ backgroundColor: "#5078ce", padding: 20, paddingTop: 60 }}>
-      <View style={{ display: "flex", flexDirection: "row" }}>
+    <View style={styles.container}>
+      <View
+        style={styles.subContainer}
+      >
         <Image
           source={{ uri: user?.user?.imageUrl }}
-          style={{ width: 45, height: 45, borderRadius: 24, padding: 8 }}
+          style={styles.image}
         />
-        <View style={{ padding: 8, marginBottom: 8 }}>
-          <Text>Welcome </Text>
-          <Text>{user.user?.fullName}</Text>
+        <View style={styles.nameContainer}>
+          <Text style={{ color: "white" }}>Welcome </Text>
+          <Text style={{ color: "white" }}>{user.user?.fullName}</Text>
         </View>
       </View>
-      <View >
+      <View style={styles.searchBar}>
         <SearchBar />
       </View>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#5078ce", padding: 20, paddingTop: 60 
+  },
+  subContainer: {
+    display: "flex",
+    flexDirection: "row",
+    alignItems: "center",
+    alignContent: "center",
+  },
+  image:{
+    width: 45, height: 45, borderRadius: 24, padding: 8 
+  },
+  nameContainer:{
+    padding: 8
+  },
+  searchBar:{
+    marginTop: 8
+  }
+})
